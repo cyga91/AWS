a78ccbee87fc3dc03be55ccebd83a977ce2cbd01
radek <radcyg91@gmail.com>
2020-09-09 18:01:01 +0200

    Add ReadWriteS3LambdaSQS.class

--- src/main/java/helloworld/s3_lambda_sqs/ReadWriteS3LambdaSQS.java
+++ src/main/java/helloworld/s3_lambda_sqs/ReadWriteS3LambdaSQS.java
@@ -1,5 +1,21 @@
 package helloworld.s3_lambda_sqs;
 
-public class ReadWriteS3LambdaSQS {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.logging.Handler;
+
+public class ReadWriteS3LambdaSQS implements RequestHandler<SQSEvent, String> {
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    private static final Logger logger = LoggerFactory.getLogger(Handler.class);
+
+    @Override
+    public String handleRequest(SQSEvent sqsEvent, Context context) {
+        return null;
+    }
 }
