e5dd5ddcb30d1ef1b7b669806a350f9e76b95c9e
radek <radcyg91@gmail.com>
2020-09-09 15:54:32 +0200

    Add file with constants.

---
+++ src/main/java/helloworld/constant/Constants.java
@@ -0,0 +1,9 @@
+package helloworld.constant;
+
+public final class Constants {
+    public static final String QUEUE_NAME = "SQSS3Lambda";
+    public static final int WITH_DELAY_SECONDS = 10;
+
+    public static final String MESSAGE = "Hello from message ";
+    public static final String MESSAGE_SENT = "Message has been sent to queue";
+}
