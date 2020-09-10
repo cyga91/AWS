5baabc97bdaeaea25a0698eb92325c51fd67cbb8
radek <radcyg91@gmail.com>
2020-09-10 13:46:06 +0200

    Add invoke of lambda to main method.

--- src/main/java/helloworld/HelloWord.java
+++ src/main/java/helloworld/HelloWord.java
@@ -1,9 +1,13 @@
 package helloworld;
 
+import helloworld.s3_lambda_sqs.Lambda;
 import helloworld.s3_lambda_sqs.S3Bucket;
+
+import static helloworld.constant.Constants.LAMBDA_FUNCTION_NAME;
 
 public class HelloWord {
     public static void main(String[] args) {
+        Lambda.createLambdaFunction(LAMBDA_FUNCTION_NAME);
         S3Bucket.putObjectToBucket();
     }
 }
