a1529a6b3be25e9a099efda0fb7f016668378b1a
radek <radcyg91@gmail.com>
2020-09-12 16:40:23 +0200

    Refactore main method.

--- src/main/java/helloworld/HelloWord.java
+++ src/main/java/helloworld/HelloWord.java
@@ -1,5 +1,8 @@
 package helloworld;
 
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import helloworld.s3_lambda_sqs.Lambda;
 import helloworld.s3_lambda_sqs.S3Bucket;
 
@@ -8,7 +11,10 @@ import static helloworld.constant.Constants.LAMBDA_FUNCTION_NAME;
 
 public class HelloWord {
     public static void main(String[] args) {
+        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.EU_WEST_1).build();
+
+
         Lambda.createLambdaFunction(LAMBDA_FUNCTION_NAME);
-        S3Bucket.putObjectToBucket(BUCKET_NAME_INPUT);
+        S3Bucket.putObjectToBucket(s3, BUCKET_NAME_INPUT);
     }
 }
