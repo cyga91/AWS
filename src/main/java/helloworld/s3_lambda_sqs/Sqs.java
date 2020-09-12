ca2a79871bd623b9f40a3cde0dbf6ad404d062be
radek <radcyg91@gmail.com>
2020-09-12 17:16:39 +0200

    Add Sqs.class with create queue and send message function.

--- src/main/java/helloworld/s3_lambda_sqs/Sqs.java
+++ src/main/java/helloworld/s3_lambda_sqs/Sqs.java
@@ -1,7 +1,6 @@
 package helloworld.s3_lambda_sqs;
 
 import com.amazonaws.services.sqs.AmazonSQS;
-import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import com.amazonaws.services.sqs.model.AmazonSQSException;
 import com.amazonaws.services.sqs.model.CreateQueueResult;
 import com.amazonaws.services.sqs.model.SendMessageBatchRequest;
@@ -13,26 +12,32 @@ import org.slf4j.LoggerFactory;
 
 import static helloworld.constant.Constants.MESSAGE;
 import static helloworld.constant.Constants.MESSAGE_SENT;
-import static helloworld.constant.Constants.QUEUE_NAME;
+import static helloworld.constant.Constants.SQS_FOUND;
 import static helloworld.constant.Constants.WITH_DELAY_SECONDS;
 
 public class Sqs {
     Gson gson = new GsonBuilder().setPrettyPrinting().create();
     private static final Logger logger = LoggerFactory.getLogger(Sqs.class);
 
-    public static void main(String[] args) {
-        final AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
-
+    private static CreateQueueResult getAmazonSQS(AmazonSQS sqs, String queueName) {
         // Create queue if doesn't exists
+        CreateQueueResult queue = null;
         try {
-            CreateQueueResult create_result = sqs.createQueue(QUEUE_NAME);
+            queue = sqs.createQueue(queueName);
         } catch (AmazonSQSException e) {
             if (!e.getErrorCode().equals("QueueAlreadyExists")) {
                 logger.error(e.getMessage());
             }
+            logger.error(e.getErrorMessage());
+            System.exit(1);
         }
+        logger.info(SQS_FOUND + queueName);
+        return queue;
+    }
 
-        String queueUrl = sqs.getQueueUrl(QUEUE_NAME).getQueueUrl();
+    private static void sendSQSMessage(AmazonSQS sqs, String queueName) {
+        getAmazonSQS(sqs, queueName);
+        String queueUrl = sqs.getQueueUrl(queueName).getQueueUrl();
 
         // Send multiple messages to the queue
         SendMessageBatchRequest send_batch_request = new SendMessageBatchRequest()
