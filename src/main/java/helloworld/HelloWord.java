2d05ecf07f862280aa3a64d6fcc86d6aea2eb277
radek <radcyg91@gmail.com>
2020-09-12 17:20:21 +0200

    Add sqs methods to main method.

--- src/main/java/helloworld/HelloWord.java
+++ src/main/java/helloworld/HelloWord.java
@@ -3,18 +3,23 @@ package helloworld;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import helloworld.s3_lambda_sqs.Lambda;
 import helloworld.s3_lambda_sqs.S3Bucket;
+import helloworld.s3_lambda_sqs.Sqs;
 
 import static helloworld.constant.Constants.BUCKET_NAME_INPUT;
 import static helloworld.constant.Constants.LAMBDA_FUNCTION_NAME;
+import static helloworld.constant.Constants.QUEUE_NAME;
 
 public class HelloWord {
     public static void main(String[] args) {
         final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.EU_WEST_1).build();
+        final AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
 
-
-        Lambda.createLambdaFunction(LAMBDA_FUNCTION_NAME);
         S3Bucket.putObjectToBucket(s3, BUCKET_NAME_INPUT);
+        Sqs.getAmazonSQS(sqs, QUEUE_NAME);
+        Lambda.createLambdaFunction(LAMBDA_FUNCTION_NAME);
     }
 }
