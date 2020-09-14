0de66e1c6c23bf1610f1cef2b115f446cd06bde6
radek <radcyg91@gmail.com>
2020-09-14 18:21:05 +0200

    Change loggers to LambdaLogger.

--- src/main/java/helloworld/s3_lambda/ReadWriteS3Lambda.java
+++ src/main/java/helloworld/s3_lambda/ReadWriteS3Lambda.java
@@ -3,35 +3,30 @@ package helloworld.s3_lambda;
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
 import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.S3Object;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.logging.Handler;
 
 public class ReadWriteS3Lambda implements RequestHandler<S3Event, String> {
-    Gson gson = new GsonBuilder().setPrettyPrinting().create();
-    private static final Logger logger = LoggerFactory.getLogger(Handler.class);
-
     @Override
     public String handleRequest(S3Event s3event, Context context) {
+        LambdaLogger logger = context.getLogger();
+
         S3EventNotification.S3EventNotificationRecord record = s3event.getRecords().get(0);
         String bucketNameOutput = record.getS3().getBucket().getName();
         String fileKeyOutput = record.getS3().getObject().getUrlDecodedKey();
 
-        String bucketNameInput = "radekincloudbucketinput";
-        String file_name = "instr.txt";
+        String bucketNameInput = "radek-in-cloud-b3";
+        String file_name = "instr1.txt";
         StringBuilder result = new StringBuilder();
 
         // read from S3
@@ -43,9 +38,9 @@ public class ReadWriteS3Lambda implements RequestHandler<S3Event, String> {
             reader.lines()
                     .forEach(result::append);
         } catch (final IOException e) {
-            logger.error(e.getMessage());
+            logger.log(e.getMessage());
         }
-        logger.info("File received from S3");
+        logger.log("File received from S3");
 
         // validate
         String fileKeyInput = result.toString();
@@ -56,9 +51,9 @@ public class ReadWriteS3Lambda implements RequestHandler<S3Event, String> {
         try {
             s3.putObject(bucketNameInput, file_name, validatedFileKeyInput);
         } catch (AmazonServiceException e) {
-            logger.error(e.getErrorMessage());
+            logger.log(e.getErrorMessage());
         }
-        logger.info("File saved in S3");
+        logger.log("File received from S3");
         return "Ok";
     }
 }
--- src/main/java/helloworld/s3_lambda_sqs/ReadWriteS3LambdaSQS.java
+++ src/main/java/helloworld/s3_lambda_sqs/ReadWriteS3LambdaSQS.java
@@ -1,22 +1,17 @@
 package helloworld.s3_lambda_sqs;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class ReadWriteS3LambdaSQS implements RequestHandler<SQSEvent, String> {
-    Gson gson = new GsonBuilder().setPrettyPrinting().create();
-    private static final Logger logger = LoggerFactory.getLogger(ReadWriteS3LambdaSQS.class);
-
     @Override
     public String handleRequest(SQSEvent sqsEvent, Context context) {
+        LambdaLogger logger = context.getLogger();
+
         for (SQSEvent.SQSMessage msg : sqsEvent.getRecords()) {
-            logger.info("processing sqs msg in lambda");
-            logger.info(msg.getBody());
+            logger.log(msg.getBody());
         }
         return null;
     }
