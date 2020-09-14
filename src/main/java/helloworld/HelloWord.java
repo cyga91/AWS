eee907167a9b9a6f8015fa988d29a9132e6560fc
radek <radcyg91@gmail.com>
2020-09-14 22:16:20 +0200

    Change code impl due to add parametres to Sqs class.

--- src/main/java/helloworld/HelloWord.java
+++ src/main/java/helloworld/HelloWord.java
@@ -3,7 +3,6 @@ package helloworld;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import helloworld.s3_lambda_sqs.Lambda;
 import helloworld.s3_lambda_sqs.S3Bucket;
@@ -16,10 +15,10 @@ import static helloworld.constant.Constants.QUEUE_OUTPUT_NAME;
 public class HelloWord {
     public static void main(String[] args) {
         final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.EU_WEST_1).build();
-        final AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
 
         S3Bucket.putObjectToBucket(s3, BUCKET_NAME_OUTPUT);
-        Sqs.getAmazonSQS(sqs, QUEUE_OUTPUT_NAME);
+        Sqs output_queue = new Sqs(AmazonSQSClientBuilder.defaultClient(), QUEUE_OUTPUT_NAME);
+        output_queue.getAmazonSQS();
         Lambda.createLambdaFunction(LAMBDA_FUNCTION_NAME);
     }
 }
