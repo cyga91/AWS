f9f7a3b73a7a8b521a08a06ac252ca6348ea507a
radek <radcyg91@gmail.com>
2020-09-15 10:36:48 +0200

    Change static calls to creating new objects. Add new lambda.

--- src/main/java/helloworld/HelloWord.java
+++ src/main/java/helloworld/HelloWord.java
@@ -8,19 +8,25 @@ import helloworld.s3_lambda_sqs.S3Bucket;
 import helloworld.s3_lambda_sqs.Sqs;
 
 import static helloworld.constant.Constants.BUCKET_NAME_OUTPUT;
+import static helloworld.constant.Constants.BUCKET_OUTPUT_KEY;
 import static helloworld.constant.Constants.FILE_NAME;
+import static helloworld.constant.Constants.LAMBDA_FUNCTION_INPUT_HANDLER;
 import static helloworld.constant.Constants.LAMBDA_FUNCTION_INPUT_NAME;
+import static helloworld.constant.Constants.LAMBDA_FUNCTION_OUTPUT_HANDLER;
 import static helloworld.constant.Constants.LAMBDA_FUNCTION_OUTPUT_NAME;
+import static helloworld.constant.Constants.LAMBDA_FUNCTION_ROLE;
 import static helloworld.constant.Constants.QUEUE_OUTPUT_NAME;
 
 public class HelloWord {
     public static void main(String[] args) {
         S3Bucket bucket = new S3Bucket(AmazonS3ClientBuilder.standard().withRegion(Regions.EU_WEST_1).build(), BUCKET_NAME_OUTPUT);
         Sqs output_queue = new Sqs(AmazonSQSClientBuilder.defaultClient(), QUEUE_OUTPUT_NAME);
+        Lambda lambdaOutput = Lambda.builder().lambdaName(LAMBDA_FUNCTION_OUTPUT_NAME).role(LAMBDA_FUNCTION_ROLE).s3BucketName(BUCKET_NAME_OUTPUT).s3BucketKey(BUCKET_OUTPUT_KEY).handler(LAMBDA_FUNCTION_OUTPUT_HANDLER).build();
+        Lambda lambdaInput = Lambda.builder().lambdaName(LAMBDA_FUNCTION_INPUT_NAME).role(LAMBDA_FUNCTION_ROLE).s3BucketName(BUCKET_NAME_OUTPUT).s3BucketKey(BUCKET_OUTPUT_KEY).handler(LAMBDA_FUNCTION_INPUT_HANDLER).build();
 
         bucket.putObjectToBucket(FILE_NAME);
         output_queue.getAmazonSQS();
-        Lambda.createLambdaFunction(LAMBDA_FUNCTION_OUTPUT_NAME);
-        Lambda.createLambdaFunction(LAMBDA_FUNCTION_INPUT_NAME);
+        lambdaOutput.createLambdaFunction();
+        lambdaInput.createLambdaFunction();
     }
 }
