0ef0f83c99fd46e565cb0d46673504eae93c11f6
radek <radcyg91@gmail.com>
2020-09-16 14:48:05 +0200

    Add another Lambda function.

--- src/main/java/helloworld/s3_lambda_sqs/ReadWriteSQSLambdaAPI.java
+++ src/main/java/helloworld/s3_lambda_sqs/ReadWriteSQSLambdaAPI.java
@@ -1,4 +1,24 @@
 package helloworld.s3_lambda_sqs;
 
-public class ReadWriteSQSLambdaAPI {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+
+import static helloworld.constant.Constants.SQS_RECEIVED_MESSAGE;
+
+public class ReadWriteSQSLambdaAPI implements RequestHandler<SQSEvent, String> {
+    @Override
+    public String handleRequest(SQSEvent sqsEvent, Context context) {
+        LambdaLogger logger = context.getLogger();
+
+        String result = null;
+
+        for (SQSEvent.SQSMessage msg : sqsEvent.getRecords()) {
+            result = msg.getBody();
+            logger.log(SQS_RECEIVED_MESSAGE + result);
+        }
+
+        return "200 Ok";
+    }
 }
